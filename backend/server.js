76a79a1254494fea17da8a2eff2a9b92a74c56a1
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-08-21 16:49:20 +0700

    signup endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv'
-import authRoutes from './routes/auth.routes.js'
-import connectToMongoDB from './db/connect.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connect.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000 
+
 const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
-app.get('/api/',(req,res)=>{
-    res.send("hello world!");
-})
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT,()=>{
-    connectToMongoDB()
-    console.log(`server Running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`server Running on port ${PORT}`);
+});
