2ade30d028b9bc5f44b457c95abf62baf4dc346f
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-08-24 09:38:27 +0700

    create login-out function

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../../hook/useLogin.js";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { loading, login } = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center  min-w-96 mx-auto">
-      
       <div
         className=" w-full p-6 shadow-md  rounded-lg bg-clip-padding backdrop-filter
        backdrop-blur-lg bg-opacity-0"
@@ -14,15 +24,17 @@ const Login = () => {
           <span className="text-sky-500/70"> ChatApp</span>
         </h1>
 
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div>
             <label htmlFor=" username" className=" label p-2">
               <span className=" text-base label-text">Username</span>
             </label>
             <input
+              name="username"
               type="text"
               placeholder="Enter username"
               className=" w-full input input-bordered h-10"
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div>
@@ -30,20 +42,24 @@ const Login = () => {
               <span className=" text-base label-text">Password</span>
             </label>
             <input
+              name="password"
               type="password"
               placeholder="Enter password"
               className=" w-full input input-bordered h-10"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <Link
-            to='/signup'
+            to="/signup"
             className="text-sm hover:underline  hover:text-sky-400 mt-2 inline-block"
           >
             {`Don't`} have an account?
           </Link>
 
           <div>
-            <button className="btn btn-block  btn-neutral  btn-sm mt-2">Login</button>
+            <button className="btn btn-block  btn-neutral  btn-sm mt-2">
+              Login
+            </button>
           </div>
         </form>
       </div>
@@ -52,57 +68,3 @@ const Login = () => {
 };
 
 export default Login;
-
-
-
-// const Login = () => {
-//   return (
-//     <div className="flex flex-col items-center justify-center  min-w-96 mx-auto">
-//       hello
-//       <div
-//         className=" w-full p-6 shadow-md  rounded-lg bg-clip-padding backdrop-filter
-//        backdrop-blur-lg bg-opacity-0"
-//       >
-//         <h1 className="text-3xl font-semibold text-center text-gray-300">
-//           Login
-//           <span className="text-sky-600/60"> ChatApp</span>
-//         </h1>
-
-//         <form action="">
-//           <div>
-//             <label htmlFor=" username" className=" label p-2">
-//               <span className=" text-base label-text">Username</span>
-//             </label>
-//             <input
-//               type="text"
-//               placeholder="Enter username"
-//               className=" w-full input input-bordered h-10"
-//             />
-//           </div>
-//           <div>
-//             <label htmlFor=" password" className=" label">
-//               <span className=" text-base label-text">Password</span>
-//             </label>
-//             <input
-//               type="password"
-//               placeholder="Enter password"
-//               className=" w-full input input-bordered h-10"
-//             />
-//           </div>
-//           <a
-//             href="#"
-//             className="text-sm hover:underline  hover:text-sky-400 mt-2 inline-block"
-//           >
-//             {`Don't`} have an account?
-//           </a>
-
-//           <div>
-//             <button className="btn btn-block btn-sm mt-2">Login</button>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import GenderCheckbox from "./GenderCheckbox.jsx";
 import { Link } from "react-router-dom";
 import useSignup from "../../hook/useSignUp.js";
@@ -19,8 +19,8 @@ const Signup = () => {
   };
 
   const handleSubmit = async (e) => {
-  	e.preventDefault();
-  	await signup(inputs);
+    e.preventDefault();
+    await signup(inputs);
   };
 
   return (
@@ -44,7 +44,9 @@ const Signup = () => {
               placeholder="Enter Full Name"
               className=" w-full input input-bordered h-10"
               value={inputs.fullname}
-              onChange={(e)=> setInputs({...inputs,fullName:e.target.value})}
+              onChange={(e) =>
+                setInputs({ ...inputs, fullName: e.target.value })
+              }
             />
           </div>
 
@@ -57,7 +59,9 @@ const Signup = () => {
               placeholder="Enter Username"
               className=" w-full input input-bordered h-10"
               value={inputs.username}
-              onChange={(e)=> setInputs({...inputs,username:e.target.value})}
+              onChange={(e) =>
+                setInputs({ ...inputs, username: e.target.value })
+              }
             />
           </div>
 
@@ -70,7 +74,9 @@ const Signup = () => {
               placeholder="Enter Password"
               className=" w-full input input-bordered h-10"
               value={inputs.password}
-              onChange={(e)=> setInputs({...inputs,password:e.target.value})}
+              onChange={(e) =>
+                setInputs({ ...inputs, password: e.target.value })
+              }
             />
           </div>
 
@@ -83,18 +89,31 @@ const Signup = () => {
               placeholder="Confirm Password"
               className=" w-full input input-bordered h-10"
               value={inputs.confirmPassword}
-              onChange={(e)=> setInputs({...inputs,confirmPassword:e.target.value})}
+              onChange={(e) =>
+                setInputs({ ...inputs, confirmPassword: e.target.value })
+              }
             />
           </div>
-          <GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} />
-          <Link to="/login"
+          <GenderCheckbox
+            onCheckboxChange={handleCheckboxChange}
+            selectedGender={inputs.gender}
+          />
+          <Link
+            to="/login"
             className=" text-sm hover:underline hover:text-sky-400 mt-4 inline-block"
           >
             Already have an account?
           </Link>
           <div>
-            <button className="btn btn-block  btn-neutral  btn-sm mt-2">
-              Sign Up
+            <button
+              disabled={loading}
+              className="btn btn-block  btn-neutral  btn-sm mt-2"
+            >
+              {loading ? (
+                <span className=" loading loading-spinner"></span>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
         </form>
